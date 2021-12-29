ac7358aa6a2bbe9afe83e97e7595d408144d30f8
gabriel fonseca <gabrielfonsecaoficial@gmail.com>
2021-12-29 13:36:47 -0400

    feat: Autenticação com Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import {signIn, signOut, useSession} from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export const SignInButton = () => {
+  const { data: session } = useSession();
 
-  const isUserLogged = true;
-
-  return isUserLogged ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
       Gabriel Fonseca
 
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) :
-    <button type="button" className={styles.signInButton}>
+    <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
