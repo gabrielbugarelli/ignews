44f1f46c78e7750ce046fce43c14c771fb2ce520
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2022-01-08 23:46:41 -0400

    feat: Verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,14 +13,28 @@ export default NextAuth({
   ],
 
   callbacks: {
-    async signIn({ user, account, profile }): Promise<boolean> {
+    async signIn( { user } ): Promise<boolean> {
       const { email } = user;
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'), 
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'), 
+              { data: { email } }
+            ),
+            q.Get(
+              q.Index('user_by_email'),
+              q.Casefold(user.email)
+            )
           )
         );
         
