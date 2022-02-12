ed061c79046e3387fc2585b19726d6324d1db31d
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-02-12 12:49:03 -0400

    refactor(Home): Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1KBiF3I69ymOPuvi0MfYlZqs');
 
@@ -53,6 +53,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+
+    revalidate: 60 * 60 * 24 // Atualiza a página a cada 24 horas
   }
 }
