cc3c957b82d460a2d127e14210d72857bac9ca80
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-02-15 19:18:52 -0400

    feat: Webhooks do Stripe

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default ( req: NextApiRequest, res: NextApiResponse ) => {
+    console.log('Evento recebido');
+
+    res.status(200).json({
+        ok: 'deu certo brow'
+    })
+}
